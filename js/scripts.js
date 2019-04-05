d9bcadfa9609de9815daa06a0e1012441352828c
Maya Frame <mayacframe@gmail.com>
2019-04-05 14:22:02 -0700

    add more function experimentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
+//Back/Business Interface
+
+var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.","Boop.","Beep."];
+var dave = theReplacements[0];
+var boop = theReplacements[1];
+var beep = theReplacements[2];
+
+while(num > 0) {
+var foo = new Array(num);
+for(var i = 0; i < num.length; i++){
+  document.write((i + 1) + foo.length + '<br/>');
+}
+};
+console.log();
 //Front/User Interface
 $(document).ready(function() {
-  $("form#formBeep").submit(function(event) {
+  $("form#beepboop").submit(function(event) {
     event.preventDefault();
-    var myNum = parseInt($("input#userBeep").val());
+    var myNum = parseInt($("input#beepboop").val());
     $("#result").text(result);
 
   });
 });
-
-//Back/Business Interface
-var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop.", "Beep."];
-console.log(myNum);
