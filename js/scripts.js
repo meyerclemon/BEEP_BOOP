c1b8fc93dbe8feb9c5c1a4a8b9dbcf23b94a296c
Maya Frame <mayacframe@gmail.com>
2019-04-05 13:12:37 -0700

    add more function experimentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
-//Back/Business Interface
-var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop.", "Beep."];
-var userInput = $("input#formBeep").val();
-var str = userInput.toString();
-
-
-
 //Front/User Interface
 $(document).ready(function() {
   $("form#formBeep").submit(function(event) {
     event.preventDefault();
-
-    $("#result").text();
+    var myNum = parseInt($("input#userBeep").val());
+    $("#result").text(result);
 
   });
 });
+
+//Back/Business Interface
+var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop.", "Beep."];
+console.log(myNum);
