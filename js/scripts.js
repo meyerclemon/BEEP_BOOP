35e88557ec47609b189b73546849480baf72e12d
Maya Frame <mayacframe@gmail.com>
2019-04-05 13:14:56 -0700

    add save

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,12 @@
-//Back/Business Interface
-
-function userNum(userInput) {
-  return userInput;
-};
-
-
 //Front/User Interface
 $(document).ready(function() {
   $("form#formBeep").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("input#userBeep").val());
-    var result = userNum(userInput);
+    var myNum = parseInt($("input#userBeep").val());
+    $("#result").text(result);
 
-    $("#result").text(userInput);
   });
 });
+
+//Back/Business Interface
+var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop.", "Beep."];
