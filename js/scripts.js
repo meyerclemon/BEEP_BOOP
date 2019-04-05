f2e7a7e4d7be5bc65154520254055e1a47b0997a
Maya Frame <mayacframe@gmail.com>
2019-04-05 16:03:53 -0700

    add more function experimentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,45 @@
+
+
+//Front/User Interface
+$(document).ready(function() {
+  // $("form#beepboop").submit(function(event) {
+  //   event.preventDefault();
+  //   var mynum = parseInt($("input#beepboop").val());
+  //   $("#result").text(result);
+
 //Back/Business Interface
 
 var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.","Boop.","Beep."];
 var dave = theReplacements[0];
 var boop = theReplacements[1];
 var beep = theReplacements[2];
+var numbers = ["1", "2", "3"];
 
-while(num > 0) {
-var foo = new Array(num);
-for(var i = 0; i < num.length; i++){
-  document.write((i + 1) + foo.length + '<br/>');
-}
-};
-console.log();
-//Front/User Interface
-$(document).ready(function() {
-  $("form#beepboop").submit(function(event) {
-    event.preventDefault();
-    var myNum = parseInt($("input#beepboop").val());
-    $("#result").text(result);
-
-  });
+theReplacements.forEach(function(replacement){
+  numbers.forEach(function(number){
+    $("#resultList").replace("<)
+  })
+})
+//
+//  var userInput = ["beepboop"];
+//  var userInputStored = [];
+//  var ints = userInput.split("");
+//
+// console.log(ints);
+//
+// userInputStored.forEach(function(int) {
+//   if(int.includes(3)) {
+//   result = userInputStored.replace(3, dave);
+// }
 });
+});
+
+// //Loop thru form and get the information, store in a new array
+//  userInput.forEach(function(num) {
+//    userInputStored.push(#("#"+item+"Input").val().toUpperCase());
+//  });
+// //display the new Array
+// userInputStored.forEach(function(item) {
+//   $(#result).append("<li>"+item+"</li>");
+// })
+//
