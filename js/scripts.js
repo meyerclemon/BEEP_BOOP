e243ca8bb49e18b8edce6227a05f16d907ccb7c8
Maya Frame <mayacframe@gmail.com>
2019-04-06 18:57:04 -0700

    add working loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,58 @@
 //Back/Business Interface
+function doTheThing(myNum) {
 
-function doTheThing( specialNum ) {
+    let range = [];
+    let boopArray = [];
 
-  let range = [];
-  let boopArray = [];
+    for (i = 0; i <= myNum; i++)
+        range.push(i);
 
-  for(i = 0; i <= specialNum; i++)
-    range.push(i);
-
-    for(i = 0; i < range.length; i++) {
-      let arg = range[i];
-      let result = beepBoop(arg);
-      boopArray.push( result );
+    for (i = 0; i < range.length; i++) {
+        let argh = range[i];
+        let result = beepBoop(argh);
+        boopArray.push(result);
     }
 
-  return boopArray; // "I did the thing"
+    return boopArray;
 }
 
 // input: num is the thing we're searching throug
 // checkfor is the thing we're searching for
 // output is TRUE if checkfor is inside num
 function contains(num, checkFor) {
-return false;
+  let numStr = num.toString();
+  let goFish = checkFor.toString();
+  let  = .indexOf();
+
+    // return true;
 
 }
 
-  function beepBoop(beeper) {
-    var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.","Boop.","Beep."];
+function beepBoop(beeper) {
+    var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop.", "Beep."];
 
-    if( contains(beeper, 3) ) {
-      return theReplacements[0];
+    if (contains(beeper, 3)) {
+        return theReplacements[0];
     }
-    if( contains(beeper, 2) ) {
-      return theReplacements[1];
+    if (contains(beeper, 2)) {
+        return theReplacements[1];
+    }
+    if (contains(beeper, 1)) {
+        return theReplacements[2];
     }
 
     let beepStr = beeper.toString();
     return beepStr;
-  }
-
-//       else return unchanged
-//
-//       return replacementArray
+}
 
 
 
 //Front/User Interface
 $(document).ready(function() {
-  $("form#beepboop").submit(function(event) {
-    event.preventDefault();
-      let myNum = parseInt($("input#beepboop").val());
-      let myResult = doTheThing( myNum );
-    $("#resultList").text(  myResult );
-})
+    $("form#beepboop").submit(function(event) {
+        event.preventDefault();
+        let myNum = parseInt($("input#beepboop").val());
+        let myResult = doTheThing(myNum);
+        $("#resultList").text(myResult);
+    })
 });
