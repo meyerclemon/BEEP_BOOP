5e64a47e252738006da480977cc98d1d7bfd24e8
Maya Frame <mayacframe@gmail.com>
2019-04-13 16:11:15 -0700

    add new, simpler code

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,29 @@
-//Back/Business Interface
-function doTheThing(myNum) {
 
-    var range = [];
-    var boopArray = [];
+var newArray = [];
+var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.","Boop.","Beep."]
 
-    for (i = 0; i <= myNum; i++)
-        range.push(i);
-
-    for (i = 0; i < range.length; i++) {
-        var argh = range[i];
-        var result = beepBoop(argh);
-        boopArray.push(result);
+function newBeepBoop(myNum) {
+  for (i=0; i <= myNum; i++) {
+    if (String(i).includes(3)) {
+      newArray.push(theReplacements[0]);
+    } else if (String(i).includes(2)) {
+        newArray.push(theReplacements[1]);
+    } else if (String(i).includes(1)) {
+      newArray.push(theReplacements[2]);
+    } else {
+      newArray.push(i);
     }
-
-    return boopArray;
+  }
+  return newArray.join(", ");
 }
 
-// input: num is the thing we're searching throug
-// checkfor is the thing we're searching for
-// output is TRUE if checkfor is inside num
-function contains(num, checkFor) {
-  var numStr = num.toString();
-  var goFish = checkFor.toString();
-  var  = .indexOf();
-
-    // return true;
-
-}
-
-function beepBoop(beeper) {
-    var theReplacements = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop.", "Beep."];
-
-    if (contains(beeper, 3)) {
-        return theReplacements[0];
-    }
-    if (contains(beeper, 2)) {
-        return theReplacements[1];
-    }
-    if (contains(beeper, 1)) {
-        return theReplacements[2];
-    }
-
-    var beepStr = beeper.toString();
-    return beepStr;
-}
-
-
-
-//Front/User Interface
 $(document).ready(function() {
-    $("form#beepboop").submit(function(event) {
-        event.preventDefault();
-        var myNum = parseInt($("input#beepboop").val());
-        var myResult = doTheThing(myNum);
-        $("#resultList").text(myResult);
-    })
+  $('form#beepboop').submit(function(event) {
+    event.preventDefault();
+    $("#resultList").show();
+
+    var myNum = $('input#beepboop').val();
+    var result = newBeepBoop(myNum);
+    $('#resultList').text(result);
+  });
 });
